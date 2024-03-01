a26776ce52fe1c47126217cd9c2cb6f72700fd42
danielcrubens <dcr.tecnologi@gmail.com>
2024-03-01 10:45:04 -0300

    configurando projeto :fire:

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+<h1 class="font-bold text-red-300 text-5">Hello</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content:[ "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
